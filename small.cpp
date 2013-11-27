6333f2afa916ac96f4ea3c01c7505652582bea03
NekOrz <joe1256joe@gmail.com>
2013-11-27 15:32:44 +0800

    updated

--- small.cpp
+++ small.cpp
@@ -3,7 +3,7 @@
 #include<cstdlib>
 #include<curses.h>
 #include<algorithm>
-#define mid 200
+#define mid 75
 #define DEBUG
 
 using namespace std;
@@ -17,32 +17,34 @@ void init()
     curs_set(0);
     nodelay(stdscr,TRUE);
     getmaxyx(stdscr,maxY,maxX);
+    start_color();
+    init_pair(1,COLOR_RED,COLOR_BLACK);
 }
 void printcir()
 {
-    mvaddch(1,2,circle[0]);
-    mvaddch(1,4,circle[1]);
-    mvaddch(1,6,circle[2]);
-    mvaddch(1,8,circle[3]);
-    mvaddch(2,1,circle[15]);
-    mvaddch(2,9,circle[4]);
-    mvaddch(3,0,circle[14]);
-    mvaddch(3,10,circle[5]);
-    mvaddch(4,0,circle[13]);
-    mvaddch(4,10,circle[6]);
-    mvaddch(5,1,circle[12]);
-    mvaddch(5,9,circle[7]);
-    mvaddch(6,8,circle[8]);
-    mvaddch(6,6,circle[9]);
-    mvaddch(6,4,circle[10]);
-    mvaddch(6,2,circle[11]);
+    mvaddch(1,2+20,circle[0]);
+    mvaddch(1,4+20,circle[1]);
+    mvaddch(1,6+20,circle[2]);
+    mvaddch(1,8+20,circle[3]);
+    mvaddch(2,1+20,circle[15]);
+    mvaddch(2,9+20,circle[4]);
+    mvaddch(3,0+20,circle[14]);
+    mvaddch(3,10+20,circle[5]);
+    mvaddch(4,0+20,circle[13]);
+    mvaddch(4,10+20,circle[6]);
+    mvaddch(5,1+20,circle[12]);
+    mvaddch(5,9+20,circle[7]);
+    mvaddch(6,8+20,circle[8]);
+    mvaddch(6,6+20,circle[9]);
+    mvaddch(6,4+20,circle[10]);
+    mvaddch(6,2+20,circle[11]);
 }
 int main()
 {
     init();
     time_t timenow=clock();
-    int cpos=0,dotpos=1,combo=0,maxcombo=0,money=0,mult=1;
-    int m_money=50,p_money=200;
+    int cpos=0,dotpos=1,combo=0,maxcombo=0,money=0,mult=1,combo_m=1;
+    int m_money=50,p_money=200,cm_money=100;
     float prot=0.0;
     bool upgrade[20];
     char c;
@@ -69,6 +71,7 @@ int main()
         }
         refresh();
         noecho();
+
         if((c=getch())!=ERR)
         {
             switch(c)
@@ -77,20 +80,20 @@ int main()
                 if (cpos == dotpos)
                 {
                     echo();
-                    mvprintw(4,15,"                            ");
-                    mvprintw(4,15,"+%d",combo*mult);
-                    money+=(combo++)*mult;
+                    money+=(combo+=combo_m)*mult;
+                    mvprintw(4,2,"              ");
+                    mvprintw(4,2,"+%d",combo*mult);
                     maxcombo=max(maxcombo,combo);
 
                 }
                 else
                 {
                     combo=(int)(combo*prot);
-                    mvprintw(4,15,"                            ");
+                    mvprintw(4,2,"           ");
                 }
                 break;
             case 'm':
-                if(money > m_money)
+                if(money >= m_money)
                 {
                     money-=m_money;
                     m_money+=10;
@@ -98,11 +101,19 @@ int main()
                 }
                 break;
             case 'p':
-                if(money>p_money)
+                if(money>=p_money)
                 {
                     money-=p_money;
                     p_money+=50;
                     prot+=(1.0-prot)*0.05;
+                }
+                break;
+            case 'c':
+                if(money>=cm_money)
+                {
+                    money-=cm_money;
+                    cm_money+=50;
+                    combo_m++;
                 }
                 break;
             #ifdef DEBUG
@@ -114,11 +125,13 @@ int main()
             }
         }
         echo();
-        mvprintw(2,15,"%d combos!",combo);
-        mvprintw(2,30,"%d maxcombos!",maxcombo);
-        mvprintw(5,15,"%d dollars",money);
+        mvprintw(2,2,"%d combos!",combo);
+        mvprintw(2,40,"%d maxcombos!",maxcombo);
+        mvprintw(5,2,"%d dollars",money);
         mvprintw(8,2,"money (m)ultiplyer ($%d) : %d",m_money,mult);
         mvprintw(10,2,"combo (p)rotecter ($%d) : %.2f%%",p_money,prot*100);
+        mvprintw(12,2,"(c)ombo multiplyer ($%d) : %d",cm_money,combo_m);
+        mvprintw(4,40,"press space when . and O meet!");
         //mvprintw()
         refresh();
     }
